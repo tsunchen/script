a1f4f85f4d20412cf2217453aeeca20a1e6ff76e
chaoqun.chen <chen.chaoqun.ext@99cloud.net>
2019-06-10 17:02:49 +0800

    golang demo

---
+++ golang/src/06-inherit.go
@@ -0,0 +1,43 @@
+﻿package main
+
+import (
+    "fmt"
+)
+
+type Human struct {
+    name   string
+    age    int
+    gender string
+}
+
+type SuperMan struct {
+    Human
+    name string
+    level int
+}
+
+func (p *Human) setName(xn string){
+    p.name = xn
+}
+
+func (p *Human) setAge(xa int) {
+    p.age = xa
+}
+
+func (p *SuperMan) setLevel(l int) {
+    p.level = l
+}
+
+// 继承
+func main() {
+     h1 := Human{"TSunx", 38, "male"}
+     var s1 SuperMan
+     s1.Human = h1
+     s1.name = "Chaos"
+     s1.setLevel(1)
+
+     fmt.Println(h1)
+     fmt.Println(s1)
+}
+
+// F:\GoProject\src\test2079>go run test02.go
