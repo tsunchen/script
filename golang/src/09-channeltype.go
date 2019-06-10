fa2df03bc7ac67df7a22f14d420d159f2f17a8da
chaoqun.chen <chen.chaoqun.ext@99cloud.net>
2019-06-10 17:10:01 +0800

    golang demo

---
+++ golang/src/09-channeltype.go
@@ -0,0 +1,34 @@
+﻿package main
+
+import (
+    "fmt"
+    "runtime"
+    "time"
+)
+
+func sendNum(c chan int) {
+    fmt.Println("begin call sendNum...")
+    c <- 7
+    fmt.Println("end call sendNum...")
+}
+
+
+// 同步机制
+func main() {
+    // 创建一个管道
+    c := make(chan int)
+    go sendNum(c) // 启动协程
+
+    // 为了防止忘记关闭
+    defer close(c)
+
+    // 睡眠3s，为了验证写端是否阻塞
+    time.Sleep(time.Second * 3)
+    num := <- c
+
+    fmt.Println("num = ", num)
+
+    runtime.Gosched() // 保证除此以外的协程彻底结束
+}
+
+// F:\GoProject\src\test2079>go run test02.go
